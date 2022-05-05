4a4dbd40d9bc5271a9624460cdc3c6ae3edaf4ed
Gaetan <71869055+gaetanepars@users.noreply.github.com>
2022-05-05 19:22:52 +0200

    Implementation of the tile class

--- TPI_BattleBorn/Source/Engine/Tile.cs
+++ TPI_BattleBorn/Source/Engine/Tile.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace TPI_BattleBorn
 {
-    public class Tile
+    public enum TileStatus
     {
+        //Units can go through
+        Passthrough=0,
+        //Units are blocked by this
+        Solid=1,
+    }
+
+    public struct Tile
+    {
+        //Texture of the tile
+        public Texture2D texture;
+        //Status of said tile
+        public TileStatus status;
+
+        //Dimensions of the tile
+        public const int tileWidth = 60;
+        public const int tileHeight = 60;
+        public static Vector2 Size = new Vector2(tileWidth, tileHeight);
+
+        public Tile(Texture2D Texture, TileStatus Status)
+        {
+            texture = Texture;
+            status = Status;
+        }
+
     }
 }
